83e4f7e796ffbd9c315c855151fddf9dccc05afe
Simon Bengtsson <simonbengt@gmail.com>
2015-09-20 20:28:28 +0200

    2.0.6

--- dist/jspdf.plugin.autotable.js
+++ dist/jspdf.plugin.autotable.js
@@ -1,5 +1,5 @@
 /**
- * jsPDF AutoTable plugin v2.0.5
+ * jsPDF AutoTable plugin v2.0.6
  * Copyright (c) 2014 Simon Bengtsson, https://github.com/simonbengtsson/jsPDF-AutoTable
  *
  * Licensed under the MIT License.
