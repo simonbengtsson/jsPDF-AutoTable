f6e1f8cd6cc235f4a43bfcac2158b204d5af3963
Simon Bengtsson <simonbengt@gmail.com>
2016-03-18 01:45:25 +0100

    Extracted polyfills

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import jsPDF from 'jspdf';
 import {Table, Row, Cell, Column} from './models.js';
 import {Config, themes, FONT_ROW_RATIO} from './config.js';
+import './polyfills.js';
 
 var doc, // The current jspdf instance
     cursor, // An object keeping track of the x and y position of the next table cell to draw
@@ -528,35 +529,4 @@ function ellipsize(text, width, styles, ellipsizeStr) {
         text = text.substring(0, text.length - 1);
     }
     return text.trim() + ellipsizeStr;
-}
-
-// Polyfills
-if (typeof Object.assign != 'function') {
-    (function () {
-        Object.assign = function (target) {
-            'use strict';
-            if (target === undefined || target === null) {
-                throw new TypeError('Cannot convert undefined or null to object');
-            }
-
-            var output = Object(target);
-            for (var index = 1; index < arguments.length; index++) {
-                var source = arguments[index];
-                if (source !== undefined && source !== null) {
-                    for (var nextKey in source) {
-                        if (source.hasOwnProperty(nextKey)) {
-                            output[nextKey] = source[nextKey];
-                        }
-                    }
-                }
-            }
-            return output;
-        };
-    })();
-}
-
-if (!Array.isArray) {
-    Array.isArray = function(arg) {
-        return Object.prototype.toString.call(arg) === '[object Array]';
-    };
 }
---
+++ src/polyfills.js
@@ -0,0 +1,31 @@
+// mdn.io/assign#Polyfill
+if (typeof Object.assign != 'function') {
+    (function () {
+        Object.assign = function (target) {
+            'use strict';
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var output = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source !== undefined && source !== null) {
+                    for (var nextKey in source) {
+                        if (source.hasOwnProperty(nextKey)) {
+                            output[nextKey] = source[nextKey];
+                        }
+                    }
+                }
+            }
+            return output;
+        };
+    })();
+}
+
+// mdn.io/isarray#Polyfill
+if (!Array.isArray) {
+    Array.isArray = function(arg) {
+        return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+}
