7d637fee11cd3be8c01b1e688eed51012fa4559d
Simon Bengtsson <simonbengt@gmail.com>
2016-07-27 11:29:14 +0200

    Revert "Polyfills with corejs, solution to #143"
    This reverts commit c11aa01f1b653b5157e9fef416e4b99c1d11a402.

--- build.js
+++ build.js
@@ -4,7 +4,6 @@ var babel = require('rollup-plugin-babel');
 var nodeResolve = require('rollup-plugin-node-resolve');
 var commonjs = require('rollup-plugin-commonjs');
 var uglify = require("uglify-js");
-var corejsBuilder = require('core-js-builder');
 
 switch (process.argv[2]) {
     case 'develop':
@@ -21,21 +20,14 @@ switch (process.argv[2]) {
  * Build the src version of jspdf-autotable
  */
 function build(dist) {
-    Promise.all([
-        corejsBuilder({
-            modules: ['es6.array.is-array', 'es6.array.iterator', 'es6.object.assign', 'es6.symbol', 'es7.object.values']
-        }),
-        rollup.rollup({
-            entry: 'src/main.js',
-            plugins: [nodeResolve({jsnext: true, main: true, skip: ['jspdf']}), commonjs({}), babel({
-                exclude: 'node_modules/**',
-                presets: ["es2015-rollup"]
-            })]
-        })
-    ]).then(function(result) {
-        var polyfills = result[0];
-        var bundle = result[1];
-        var code = polyfills + bundle.generate({
+    rollup.rollup({
+        entry: 'src/main.js',
+        plugins: [nodeResolve({jsnext: true, main: true, skip: ['jspdf']}), commonjs({}), babel({
+            exclude: 'node_modules/**',
+            presets: ["es2015-rollup"]
+        })]
+    }).then(function (bundle) {
+        var code = bundle.generate({
             format: 'umd',
             banner: '/** \n' +
             ' * jsPDF AutoTable plugin v' + require('./package.json').version + '\n' +
@@ -53,12 +45,12 @@ function build(dist) {
             fs.writeFileSync('./dist/jspdf.plugin.autotable.js', minified);
             fs.writeFileSync('./dist/jspdf.plugin.autotable.src.js', code);
         }
-        fs.writeFileSync('./examples/libs/jspdf.plugin.autotable.src.js', code);
+        fs.writeFileSync( './examples/libs/jspdf.plugin.autotable.src.js', code);
         var write = fs.createWriteStream('./examples/libs/jspdf.min.js');
         fs.createReadStream('./node_modules/jspdf/dist/jspdf.min.js').pipe(write);
 
         console.log('Done');
-    }).catch(function (err) {
+    }).catch(function(err) {
         console.log('ROLLUP ERROR:');
         console.error(err);
     });
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import jsPDF from 'jspdf';
 import {Table, Row, Cell, Column} from './models.js';
 import {Config, themes, FONT_ROW_RATIO} from './config.js';
+import './polyfills.js';
 
 var doc, // The current jspdf instance
     cursor, // An object keeping track of the x and y position of the next table cell to draw
