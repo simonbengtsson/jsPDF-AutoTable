41fd0fbbdf2c9ba63c1921d61e2024eb15dd4280
Simon Bengtsson <simonbengt@gmail.com>
2016-07-27 11:50:34 +0200

    Readd corejs polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,36 +1,5 @@
-
-// mdn.io/assign#Polyfill
-if (typeof Object.assign != 'function') {
-    (function () {
-        Object.assign = function (target) {
-            'use strict';
-            if (target === undefined || target === null) {
-                throw new TypeError('Cannot convert undefined or null to object');
-            }
-
-            var output = Object(target);
-            for (var index = 1; index < arguments.length; index++) {
-                var source = arguments[index];
-                if (source !== undefined && source !== null) {
-                    for (var nextKey in source) {
-                        if (source.hasOwnProperty(nextKey)) {
-                            output[nextKey] = source[nextKey];
-                        }
-                    }
-                }
-            }
-            return output;
-        };
-    })();
-}
-
-// mdn.io/isarray#Polyfill
-if (!Array.isArray) {
-    Array.isArray = function(arg) {
-        return Object.prototype.toString.call(arg) === '[object Array]';
-    };
-}
-
-if (!Object.values) {
-    Object.values = obj => Object.keys(obj).map(key => obj[key]);
-}
+import 'core-js/es6/symbol';
+import 'core-js/fn/array/iterator';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/array/is-array';
+import 'core-js/fn/object/values';
