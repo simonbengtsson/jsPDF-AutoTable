1f0a4c06fdd6a86f8a9fa09b9584d8dabae6eece
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2020-03-23 11:47:06 +0100

    Support non default jsPDF dist files

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@
 import applyApi from './applyApi'
 import { UserOptions } from './interfaces'
 
+export function applyPlugin(jsPDF) {
+  applyApi(jsPDF)
+}
 export type autoTable = (options: UserOptions) => void
 
-const jsPDF = require('jspdf')
-applyApi(jsPDF)
+try {
+  const jsPDF = require('jspdf')
+  applyApi(jsPDF)
+} catch(error) {
+  // Importing jspdf in nodejs environment currently does not work
+  // so we need to silence any errors
+}
