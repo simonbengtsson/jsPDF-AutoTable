bcbbe3b11c0b5f282245f9dc1edd1fcf2f5bd1d6
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2020-04-08 23:30:14 +0200

    Remove duplicate call of didParseCell hook

--- src/inputParser.ts
+++ src/inputParser.ts
@@ -259,7 +259,6 @@ function parseContent(table) {
         if (cell.colSpan > 1 && !column.wrappedWidth) {
           column.wrappedWidth = cell.minWidth
         }
-        table.callCellHooks(table.cellHooks.didParseCell, cell, row, column)
       }
     }
   })
