cd9b111c8e00f79f064f9d08ebec9850d62a3c13
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2020-04-28 14:09:07 +0200

    Remove accidentally pushed export

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@
 import _applyPlugin from './applyPlugin'
 import { UserOptions } from './config'
 import { jsPDFConstructor } from './documentHandler'
-import { autoTable as _autoTable } from './autoTable'
 
 // export { applyPlugin } didn't export applyPlugin
 // to index.d.ts for some reason
@@ -11,11 +10,6 @@ export function applyPlugin(jsPDF: jsPDFConstructor) {
   _applyPlugin(jsPDF)
 }
 export type autoTable = (options: UserOptions) => void
-
-export function autoTable(jsPDF: jsPDFConstructor, options: UserOptions) {
-  _applyPlugin(jsPDF)
-  _autoTable(options)
-}
 
 try {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
