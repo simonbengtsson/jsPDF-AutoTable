2d328f124051f8a10b2c39714a8601a5e2d62225
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2020-05-01 01:34:02 +0200

    Export experimental create and draw table api

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import _applyPlugin from './applyPlugin'
 import { UserOptions } from './config'
 import { jsPDFConstructor, jsPDFDocument } from './documentHandler'
 import { parseInput } from './inputParser'
-import { drawTable } from './tableDrawer'
-import { createTable } from './tableCalculator'
+import { drawTable as _drawTable } from './tableDrawer'
+import { createTable as _createTable } from './tableCalculator'
+import { Table } from './models'
 
 export type autoTable = (options: UserOptions) => void
 
@@ -17,8 +18,18 @@ export function applyPlugin(jsPDF: jsPDFConstructor) {
 
 export default function autoTable(d: jsPDFDocument, options: UserOptions) {
   const input = parseInput(d, options)
-  const table = createTable(d, input)
-  drawTable(d, table)
+  const table = _createTable(d, input)
+  _drawTable(d, table)
+}
+
+// Experimental export
+export function __createTable(d: jsPDFDocument, options: UserOptions): Table {
+  const input = parseInput(d, options)
+  return _createTable(d, input)
+}
+
+export function __drawTable(d: jsPDFDocument, table: Table) {
+  _drawTable(d, table)
 }
 
 try {
