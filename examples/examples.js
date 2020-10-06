379c0195f023de94766e4aaad2e80ac8f9996aa3
prpateldev <prpateldev@gmail.com>
2020-10-06 19:40:32 +0530

    changing example name to Horizontal Page Break from Split Pages

--- examples/examples.js
+++ examples/examples.js
@@ -503,7 +503,7 @@ examples.custom = function () {
 }
 
 // Split columns - shows how the overflowed columns split into pages
-examples.splitPages = function () {
+examples.horizontalPageBreak = function () {
   var doc = new jsPDF('l')
 
   var head = headRows()
