6e4720f98c9e98500d546891c6526b8d262e90da
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2021-02-12 18:43:22 +0100

    Add cloned table test

--- test/testHtmlParser.ts
+++ test/testHtmlParser.ts
@@ -34,6 +34,33 @@ describe('html parser', () => {
     assert(res.foot[0].length, 'Should have foot cell')
   })
 
+  it('cloned table', () => {
+    const doc = new DocHandler(new jsPDF())
+    const table = dom.window.document.createElement('table')
+    let section = table.createTBody()
+    let row = section.insertRow()
+    let cell1 = row.insertCell()
+    cell1.innerHTML = '<p>test</p>'
+    let cell2 = row.insertCell()
+    cell2.innerHTML = '<p>test</p>'
+
+    section = table.createTHead()
+    row = section.insertRow()
+    cell1 = row.insertCell()
+    cell1.innerHTML = '<p>test</p>'
+
+    section = table.createTFoot()
+    row = section.insertRow()
+    cell1 = row.insertCell()
+    cell1.innerHTML = '<p>test</p>'
+
+    const res = parseHtml(doc, table.cloneNode(true), dom.window)
+    assert(res, 'Should have result')
+    assert(res.head[0].length, 'Should have head cell')
+    assert.equal(res.body[0].length, 2, 'Should have two body cells')
+    assert(res.foot[0].length, 'Should have foot cell')
+  })
+
   it('hidden content', () => {
     const doc = new DocHandler(new jsPDF())
     const table = dom.window.document.createElement('table')
