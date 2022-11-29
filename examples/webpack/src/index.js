133c5ffeca8464c990b3cf2d4d3f07459b176490
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2022-11-29 19:48:43 +0100

    Simplify webpack example

--- examples/webpack/src/index.js
+++ examples/webpack/src/index.js
@@ -1,4 +1,4 @@
-import jsPDF from 'jspdf'
+import { jsPDF } from 'jspdf'
 import 'jspdf-autotable'
 
 document.getElementById('pdf-button').onclick = function () {
--- examples/webpack/webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mode: 'development',
-  entry: './main.js',
-  output: {
-    filename: 'bundle.js',
-  }
-}
